924ca93058e7ef1f4c285e0b7265204a7e8f6614
Mike <mihailoarr@ya.ru>
2024-08-25 22:51:22 +0500

    LoginSuccessHandler

--- src/main/java/ru/kata/spring/boot_security/demo/config/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/config/SuccessUserHandler.java
@@ -1,25 +1,34 @@
 package ru.kata.spring.boot_security.demo.config;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Set;
+import java.util.Collection;
 
 @Component
 public class SuccessUserHandler implements AuthenticationSuccessHandler {
-    // Spring Security использует объект Authentication, пользователя авторизованной сессии.
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
-        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/user");
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String redirectUrl = determineRedirectUrl(authentication);
+        response.sendRedirect(redirectUrl);
+    }
+
+    private String determineRedirectUrl(Authentication authentication) {
+        Collection<? extends SimpleGrantedAuthority> authorities = (Collection<? extends SimpleGrantedAuthority>) authentication.getAuthorities();
+
+        // Проверяем, есть ли у пользователя роль ADMIN
+        if (authorities.stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
+            return "/admin"; // Перенаправление для администраторов
+        } else if (authorities.stream().anyMatch(a -> a.getAuthority().equals("USER"))) {
+            return "/user"; // Перенаправление для пользователей
         } else {
-            httpServletResponse.sendRedirect("/");
+            return "/"; // По умолчанию, если ни одной роли нет
         }
     }
 }
